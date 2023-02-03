25e1b2bbf4ab725184ce321d339f08516e26ef4c
GaMeNu <98153342+GaMeNu@users.noreply.github.com>
2023-02-03 10:33:25 +0200

    Plugin now caches player in memory when logging in and removes when logging out

--- src/main/java/me/gamenu/griefdetector/MyListener.java
+++ src/main/java/me/gamenu/griefdetector/MyListener.java
@@ -8,17 +8,22 @@ import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.sql.*;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MyListener implements Listener {
+
+    private static final ConcurrentHashMap<UUID, Integer> playerCache = new ConcurrentHashMap<>();
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
-        Connection conn = null;
-        PreparedStatement statement = null;
+        Connection conn;
+        PreparedStatement statement;
 
         try {
             //Check if player already exists in DB
@@ -27,17 +32,20 @@ public class MyListener implements Listener {
             checkStmt.setString(1, event.getPlayer().getUniqueId().toString());
             ResultSet rs = checkStmt.executeQuery();
 
+            int bwuId;
 
             if (rs.next()){
                 //Player in DB
-                int givenId = rs.getInt(EnvVars.ID_COLUMN);
-                event.getPlayer().sendMessage(Component.text("Hello there, " + event.getPlayer().getName() +"! (ID:" + givenId + ")"));
+                bwuId = rs.getInt(EnvVars.ID_COLUMN);
+                event.getPlayer().sendMessage(Component.text("Hello there, " + event.getPlayer().getName() +"! (ID:" + bwuId + ")"));
                 checkStmt = conn.prepareStatement("SELECT username FROM players WHERE uuid = ?;");
                 checkStmt.setString(1, event.getPlayer().getUniqueId().toString());
                 rs = checkStmt.executeQuery();
+
                 if (!rs.next()){
-                    statement = conn.prepareStatement("INSERT INTO players (username) VALUES (?);");
+                    statement = conn.prepareStatement("INSERT INTO players (username,uuid) VALUES (?,?);");
                     statement.setString(1, event.getPlayer().getName());
+                    statement.setString(2, event.getPlayer().getUniqueId().toString());
                     event.getPlayer().getServer().getLogger().info("Update username for player");
 
                 } else if (!rs.getString("username").equals(event.getPlayer().getName())) {
@@ -60,13 +68,16 @@ public class MyListener implements Listener {
                 //Gotta be nice, you know
                 checkStmt = conn.prepareStatement("SELECT " + EnvVars.ID_COLUMN + " FROM players WHERE uuid = ?;");
                 checkStmt.setString(1, event.getPlayer().getUniqueId().toString());
-                int givenId = checkStmt.executeQuery().getInt(EnvVars.ID_COLUMN);
+                bwuId = checkStmt.executeQuery().getInt(EnvVars.ID_COLUMN);
 
 
-                event.getPlayer().sendMessage(Component.text("Welcome, "+event.getPlayer().getName()+"! (ID:"+givenId+")"));
+                event.getPlayer().sendMessage(Component.text("Welcome, "+event.getPlayer().getName()+"! (ID:"+bwuId+")"));
             }
 
+            playerCache.put(event.getPlayer().getUniqueId(), bwuId);
+            event.getPlayer().getServer().getLogger().info("Player " + event.getPlayer().getName() + " was put to cache");
             conn.close();
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -74,22 +85,24 @@ public class MyListener implements Listener {
 
     }
 
+    @EventHandler
+    private void playerQuitEvent(PlayerQuitEvent event){
+        playerCache.remove(event.getPlayer().getUniqueId());
+        event.getPlayer().getServer().getLogger().info("Player " + event.getPlayer().getName() + " was removed from the cache");
+    }
+
+
     private void updateDBWithBlock(Event event, Player player, Block block){
         if (!(event instanceof BlockPlaceEvent) && !(event instanceof BlockBreakEvent)) {
             return;
         }
 
-        Connection conn = null;
-        PreparedStatement statement = null;
-
-        try{
-            conn = DriverManager.getConnection(EnvVars.DB_URL);
+        PreparedStatement statement;
+        try (Connection conn = DriverManager.getConnection(EnvVars.DB_URL)) {
 
             //Get the player's BWU ID
-            PreparedStatement checkStmt = conn.prepareStatement("SELECT " + EnvVars.ID_COLUMN + " FROM players WHERE uuid = ?;");
-            checkStmt.setString(1, player.getUniqueId().toString());
-            ResultSet rs = checkStmt.executeQuery();
-            int bwuId = rs.getInt(EnvVars.ID_COLUMN);
+            ResultSet rs;
+            int bwuId = playerCache.get(player.getUniqueId());
 
             //Get the record for current key "(x,y,z)" and check if exists
             Location coords = block.getLocation();
@@ -98,12 +111,12 @@ public class MyListener implements Listener {
             statement.setString(1, key);
             rs = statement.executeQuery();
 
-            PreparedStatement modifyRecord = null;
+            PreparedStatement modifyRecord;
 
             //Check if record exists for key
-            if (!rs.next()){
+            if (!rs.next()) {
                 //Create new record
-                modifyRecord = conn.prepareStatement("INSERT INTO gd_store (coords, "+ EnvVars.ID_COLUMN +") VALUES (?,?);");
+                modifyRecord = conn.prepareStatement("INSERT INTO gd_store (coords, " + EnvVars.ID_COLUMN + ") VALUES (?,?);");
                 modifyRecord.setString(1, key);
                 modifyRecord.setInt(2, bwuId);
 
@@ -116,12 +129,9 @@ public class MyListener implements Listener {
 
 
             modifyRecord.executeUpdate();
-            conn.close();
 
         } catch (Exception e) {
             throw new RuntimeException(e);
-
-
         }
 
     }
